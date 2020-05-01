cea3f02efa837d2fec2e517a2f0fb9816ae8ec75
chayadeaver <chayadeaver@gmail.com>
2020-05-01 09:12:30 -0400

    added items rotue

--- project-backend/menu-api/config/routes.rb
+++ project-backend/menu-api/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :menus do
     resources :items
   end
+  resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
