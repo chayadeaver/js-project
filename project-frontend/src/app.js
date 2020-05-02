19d985c30e004425371112333e00b80123bcbb46
chayadeaver <chayadeaver@gmail.com>
2020-05-01 23:27:02 -0400

    wrote method to fetch items from backend

--- project-frontend/src/app.js
+++ project-frontend/src/app.js
@@ -1,3 +1,19 @@
 class App{
-    
+    items = []
+    menus = []
+    url = "http://localhost:3000"
+
+    getItems() {
+        // make a fetch request to /items
+        fetch(`${this.url}/items`)
+            .then(resp => resp.json())
+            .then(data => console.log(data))
+        // populate the items and menus properties with the returned data
+        // call renderItems
+        .catch(err => alert(err))
+    }
+
+    renderItems(){
+        // create DOM nodes and insert data into them to render in the DOM
+    }
 }
