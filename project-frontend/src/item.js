a6e3a93f4e45acaa41e3b474d4b8ab78d43e6bcc
chayadeaver <chayadeaver@gmail.com>
2020-05-01 22:50:49 -0400

    created Item class

---
+++ project-frontend/src/item.js
@@ -0,0 +1,8 @@
+class Item {
+    constructor(name, price, description, image_url){
+        this.name = name
+        this.price = price
+        this.description = description
+        this.image_url = image_url
+    }
+}
