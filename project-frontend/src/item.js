ce43030333ca6888207025ffe432fbbc08d23fe6
chayadeaver <chayadeaver@gmail.com>
2020-05-03 10:24:26 -0400

    added parameters for menu

--- project-frontend/src/item.js
+++ project-frontend/src/item.js
@@ -1,8 +1,12 @@
 class Item {
-    constructor(name, price, description, image_url){
+    constructor(name, price, description, image_url, id, menu_id, menu_name){
         this.name = name
         this.price = price
         this.description = description
         this.image_url = image_url
+        this.id = id
+        this.menu_id = menu_id
+        this.menu_name = menu_name
+
     }
 }
