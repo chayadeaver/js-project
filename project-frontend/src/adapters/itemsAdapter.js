3197a164323ff53d177e683e35769366963e1b73
chayadeaver <chayadeaver@gmail.com>
2020-05-05 23:18:43 -0400

    added post and delete fetch

--- project-frontend/src/adapters/itemsAdapter.js
+++ project-frontend/src/adapters/itemsAdapter.js
@@ -8,5 +8,26 @@ class ItemsAdapter {
         .then(resp => resp.json())
         .then(json => json.data)
     }
+
+    postItems(data) {
+        return fetch(this.baseURL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(resp => resp.json())
+    }
+
+    deleteItems(id) {
+        return fetch(`${this.baseURL}/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(resp => resp.json())
+    }
 }
 
